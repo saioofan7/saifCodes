0d3fa6a8cbdc026c7af2b038eeea3e3e5a515f94
SaifAlnsour <saifthelizard@gmail.com>
2024-01-19 19:54:02 +0300

    finished minimal setup for first project

---
+++ Saioofan7First/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Saioofan7First
+//
+//  Created by Saif Alnsour on 19/01/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Saif Alnsour")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Saioofan7First/Saioofan7FirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  Saioofan7FirstApp.swift
+//  Saioofan7First
+//
+//  Created by Saif Alnsour on 19/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Saioofan7FirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
